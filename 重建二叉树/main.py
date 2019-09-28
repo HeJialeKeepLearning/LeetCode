d0a2f4f0be2a8367878843b06c65033d14bc648e
HeJialeKeepLearning <35835341+HeJialeKeepLearning@users.noreply.github.com>
2019-09-28 15:43:50 +0800

    NewCoder
    二叉树

--- 重建二叉树/main.py
+++ 重建二叉树/main.py
@@ -1,11 +1,18 @@
-def reConstructBinaryTree(pre, tin):
-    # write code here
-    length = len(pre)
-    if length == 0: # 终止条件
-        return None
-    root = TreeNode(pre[0])  # 记录根节点
-    if root is not None:
-        rootIndex = tin.index(root.val)  # 找到根节点在中序遍历中的索引index
-        root.left = reConstructBinaryTree(pre[1:1 + rootIndex], tin[:rootIndex])
-        root.right = reConstructBinaryTree(pre[rootIndex + 1:], tin[rootIndex + 1:])
-    return root
+# -*- coding:utf-8 -*-
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+class Solution:
+    # 返回构造的TreeNode根节点
+    def reConstructBinaryTree(self, pre, tin):
+        # write code here
+        if not pre:
+            return None
+        root = TreeNode(pre[0])  # 记录根节点
+        if root:
+            rootIndex = tin.index(root.val)  # 找到根节点在中序遍历中的索引index
+            root.left = self.reConstructBinaryTree(pre[1:1 + rootIndex], tin[:rootIndex])
+            root.right = self.reConstructBinaryTree(pre[rootIndex + 1:], tin[rootIndex + 1:])
+        return root
