b0750b635ebc16ce0d1f8417680ab9866e61bc0c
HeJialeKeepLearning <35835341+HeJialeKeepLearning@users.noreply.github.com>
2019-09-28 18:09:40 +0800

    NewCoder
    递推

--- 矩形覆盖/main.py
+++ 矩形覆盖/main.py
@@ -1,12 +1,11 @@
-def rectCover(number):
-    if number==0:
-        return 0
-    if number==1:
-        return 1
-    if number==2:
-        return 2
-    k,a,b=2,1,2
-    while(k<number):
-        a,b=b,a+b
-        k+=1
-    return b
+# -*- coding:utf-8 -*-
+class Solution:
+    def rectCover(self, number):
+        if number == 0:
+            return 0
+        a, b = 1, 2
+        number -= 1
+        while number:
+            a, b = b, a + b
+            number -= 1
+        return a
