d95e11d6ef0d30a6b062a0e62c3f5515d569a459
HeJialeKeepLearning <35835341+HeJialeKeepLearning@users.noreply.github.com>
2019-09-30 17:41:17 +0800

    nowcoder
    链表

--- 反转链表/main.py
+++ 反转链表/main.py
@@ -1,13 +1,19 @@
-def ReverseList(pHead):
-    plist = []
-    p = pHead
-    while p is not None:
-        plist.append(p.val)
-        p = p.next
-    plist.reverse()
-    p = pHead
-    for i in plist:
-        p.val = i
-        p = p.next
-
-    return pHead
+# -*- coding:utf-8 -*-
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+class Solution:
+    # 返回ListNode
+    def ReverseList(self, pHead):
+        val_list = []
+        p = pHead
+        while p:
+            val_list.append(p.val)
+            p = p.next
+        val_list.reverse()
+        p = pHead
+        for val in val_list:
+            p.val = val
+            p = p.next
+        return pHead
