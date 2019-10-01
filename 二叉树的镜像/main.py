0a220f3eb35e8a145e30d16f69ad1c14d3a64a49
HeJialeKeepLearning <35835341+HeJialeKeepLearning@users.noreply.github.com>
2019-10-01 09:44:24 +0800

    nowcoder
    树

--- 二叉树的镜像/main.py
+++ 二叉树的镜像/main.py
@@ -1,13 +1,18 @@
-def Mirror(root):
-
-    if root is None:
+# -*- coding:utf-8 -*-
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution:
+    # 返回镜像树的根节点
+    def Mirror(self, root):
+        if not root:
+            return root
+        if root.left or root.right:
+            root.left, root.right = root.right, root.left
+        if root.left:
+            self.Mirror(root.left)
+        if root.right:
+            self.Mirror(root.right)
         return root
-
-    if root.left is not None or root.right is not None:  # 有子树
-        root.left, root.right = root.right, root.left
-    if root.left is not None:
-        Mirror(root.left)
-    if root.right is not None:
-        Mirror(root.right)
-
-    return root
