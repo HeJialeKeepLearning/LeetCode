fa746694252debf7eaeced2b0b8a6eda9aa80bb5
HeJialeKeepLearning <35835341+HeJialeKeepLearning@users.noreply.github.com>
2019-10-01 11:29:44 +0800

    nowcoder
    树的层次遍历

--- 从上往下打印二叉树/main.py
+++ 从上往下打印二叉树/main.py
@@ -1,16 +1,23 @@
-def PrintFromTopToBottom(root):
-    if root is None:
-        return []
-    queue = []
-    resultlist = []
-    p = root
-    queue.append(p)
-    while len(queue)!=0:
-        p = queue.pop(0)
-        resultlist.append(p.val)
-        if p.left is not None:
-            queue.append(p.left)
-        if p.right is not None:
-            queue.append(p.right)
+# -*- coding:utf-8 -*-
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+class Solution:
+    # 返回从上到下每个节点值列表，例：[1,2,3]
+    def PrintFromTopToBottom(self, root):
+        if not root:
+            return []
+        ret_list = []
+        queue = []
+        queue.append(root)
+        while queue:
+            p = queue.pop(0)
+            ret_list.append(p.val)
+            if p.left:
+                queue.append(p.left)
+            if p.right:
+                queue.append(p.right)
+        return ret_list
 
-    return resultlist
