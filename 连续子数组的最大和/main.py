bfb58d1df93a9e1621b6106058bbc696a2681c39
HeJialeKeepLearning <35835341+HeJialeKeepLearning@users.noreply.github.com>
2019-10-02 21:47:38 +0800

    nowcoder
    经典简单动态规划

--- 连续子数组的最大和/main.py
+++ 连续子数组的最大和/main.py
@@ -1,11 +1,12 @@
-def FindGreatestSumOfSubArray(array):
-    max = array[0]
-    cnt = array[0]
-    for i in array[1:]:
-        if cnt > 0:
-            cnt += i
-        else:
-            cnt = i
-        if max < cnt:
-            max = cnt
-    return max
+# -*- coding:utf-8 -*-
+class Solution:
+    def FindGreatestSumOfSubArray(self, array):
+        cur_sum = array[0]
+        ret_sum = array[0]
+        for element in array[1:]:
+            if cur_sum > 0:
+                cur_sum += element
+            else:
+                cur_sum = element
+            ret_sum = max(ret_sum, cur_sum)
+        return ret_sum
