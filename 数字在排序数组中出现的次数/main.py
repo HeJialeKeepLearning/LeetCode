d083b406396fbf61d31a72d563bb647f9a4bdaf3
HeJialeKeepLearning <35835341+HeJialeKeepLearning@users.noreply.github.com>
2019-10-03 22:07:50 +0800

    nowcoder
    数组、查找

--- 数字在排序数组中出现的次数/main.py
+++ 数字在排序数组中出现的次数/main.py
@@ -1,10 +1,10 @@
-def GetNumberOfK(data, k):
-    try:
-        kindex = data.index(k)
-        cnt = 1
-        for i in range(kindex + 1, len(data)):
-            if data[i] == k:
+# -*- coding:utf-8 -*-
+class Solution:
+    def GetNumberOfK(self, data, k):
+        cnt = 0
+        for each_data in data:
+            if cnt and (each_data != k):
+                return cnt
+            if each_data == k:
                 cnt += 1
         return cnt
-    except ValueError:
-        return 0
