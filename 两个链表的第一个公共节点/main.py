cfd4c50c9fb4533b594ac473f37e000137a7ee90
HeJialeKeepLearning <35835341+HeJialeKeepLearning@users.noreply.github.com>
2019-10-03 21:58:50 +0800

    nowcoder
    链表

--- 两个链表的第一个公共节点/main.py
+++ 两个链表的第一个公共节点/main.py
@@ -5,7 +5,6 @@
 #         self.next = None
 class Solution:
     def FindFirstCommonNode(self, pHead1, pHead2):
-        # write code here
         len1, len2 = 0, 0
         p1, p2 = pHead1, pHead2
         while p1:
@@ -14,15 +13,18 @@ class Solution:
         while p2:
             len2 += 1
             p2 = p2.next
-        while len1 - len2 > 0:
-            pHead1 = pHead1.next
-            len1 -= 1
-        while len2 - len1 > 0:
-            pHead2 = pHead2.next
-            len2 -= 1
-        while pHead1:
-            if pHead1.val == pHead2.val:
-                return pHead1
-            else:
-                pHead1 = pHead1.next
-                pHead2 = pHead2.next
+        if len1 > len2:
+            longp = pHead1
+            shortp = pHead2
+        else:
+            longp = pHead2
+            shortp = pHead1
+        diff = abs(len1 - len2)
+        while diff:
+            longp = longp.next
+            diff -= 1
+        while longp:
+            if longp == shortp:
+                return longp
+            longp = longp.next
+            shortp = shortp.next
