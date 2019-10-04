b9dbe115d849aa11a81af203ea0a1d6762a9d182
HeJialeKeepLearning <35835341+HeJialeKeepLearning@users.noreply.github.com>
2019-10-04 11:46:21 +0800

    nowcoder
    位运算（虽然没用……）

--- 数组中只出现一次的数字/main.py
+++ 数组中只出现一次的数字/main.py
@@ -1,8 +1,11 @@
-def FindNumsAppearOnce(self,array):
-    arrdic = {}
-    for i in array:
-        if i in arrdic:
-            del arrdic[i]
-        else:
-            arrdic[i] = 1
-    return list(arrdic.keys())
+# -*- coding:utf-8 -*-
+class Solution:
+    # 返回[a,b] 其中ab是出现一次的两个数字
+    def FindNumsAppearOnce(self, array):
+        element_list = []
+        for element in array:
+            if element in element_list:
+                element_list.remove(element)
+            else:
+                element_list.append(element)
+        return element_list
