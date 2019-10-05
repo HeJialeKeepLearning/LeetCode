b912b9001ea87430a01b075ce029dffe13f8194c
HeJialeKeepLearning <35835341+HeJialeKeepLearning@users.noreply.github.com>
2019-10-05 17:24:18 +0800

    nowcoder
    字符串

--- 把字符串转换成整数/main.py
+++ 把字符串转换成整数/main.py
@@ -1,23 +1,20 @@
-def StrToInt(s):
-    if len(s) == 0:
-        return 0
-    c = s[0]
-    flag = True  # 正数
-    if c == '+':
-        s = s[1:]
-    elif c == '-':
-        flag = False  # 负数
-        s = s[1:]
-    if not s.isdigit():
-        return 0
-    snum = 0
-    p = 0  # 每一位的权重
-    index = len(s) - 1
-    while index >= 0:
-        snum = snum + (ord(s[index]) - ord('0')) * pow(10, p)
-        index -= 1
-        p += 1
-    if flag:
-        return snum
-    else:
-        return -snum
+# -*- coding:utf-8 -*-
+class Solution:
+    def StrToInt(self, s):
+        if not s:
+            return 0
+        op = s[0]
+        if op in ['+', '-']:
+            s = s[1:]
+        if not s.isdigit():
+            return 0
+        num = 0
+        for char in s:
+                num = num * 10 + int(char)
+        maxint = pow(2, 31)
+        if num > maxint or (op != '-' and num > maxint - 1):
+            return 0
+        if op == '-':
+            return -num
+        return num
+
