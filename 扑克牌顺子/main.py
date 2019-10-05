de47bced3506a8e4be91fe35eed51e893392c767
HeJialeKeepLearning <35835341+HeJialeKeepLearning@users.noreply.github.com>
2019-10-05 15:20:59 +0800

    nowcoder
    简单数组

--- 扑克牌顺子/main.py
+++ 扑克牌顺子/main.py
@@ -1,18 +1,19 @@
-def IsContinuous(numbers):
-    if not numbers:
-        return False
-    numbers.sort()
-    index = 0
-    cnt = 0
-    while index < len(numbers) - 1 and cnt >= 0:
-        if numbers[index] == 0:
-            cnt += 1
-        else:
-            if numbers[index] + 1 != numbers[index + 1]:
-                cnt -= 1
+# -*- coding:utf-8 -*-
+class Solution:
+    def IsContinuous(self, numbers):
+        if not numbers:
+            return False
+        zero_cnt, index = 0, 0
+        numbers.sort()
+        while index < len(numbers) - 1:
+            if numbers[index] == 0:
+                zero_cnt += 1
+                index += 1
+                continue
+            if numbers[index] != numbers[index + 1] - 1:
+                zero_cnt -= 1
                 numbers.insert(index + 1, numbers[index] + 1)
-        index += 1
-    if cnt < 0:
-        return False
-    else:
+            if zero_cnt < 0:
+                return False
+            index += 1
         return True
