d014b942cf054bc2d048f9569a53eab69554a947
HeJialeKeepLearning <35835341+HeJialeKeepLearning@users.noreply.github.com>
2019-10-05 14:46:19 +0800

    nowcoder
    简单字符串

--- 翻转单词顺序列/main.py
+++ 翻转单词顺序列/main.py
@@ -1,7 +1,6 @@
-def ReverseSentence(s):
-    sreverselist = s.split(' ')
-    sreverselist.reverse()
-    sreverse = ''
-    for c in sreverselist:
-        sreverse = sreverse + ' ' + c
-    return sreverse[1:]
+# -*- coding:utf-8 -*-
+class Solution:
+    def ReverseSentence(self, s):
+        s_parts = s.split(' ')
+        s_parts.reverse()
+        return ' '.join(s_parts)
